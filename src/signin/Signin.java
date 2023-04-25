cb6487110d22970c3b34da545fed4e95bee0af36
Isuru Hettiarachchi <120737654+IsuruDH99@users.noreply.github.com>
2023-04-25 22:33:56 +0530

    new

---
+++ src/signin/Signin.java
@@ -0,0 +1,204 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
+ */
+package signin;
+
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import javax.swing.JOptionPane;
+import java.util.Base64;
+import java.util.Base64.Encoder;
+
+public class Signin extends javax.swing.JFrame {
+
+    Connection con = null;
+    ResultSet rs = null;
+    PreparedStatement ps = null;
+    /**
+     * Creates new form Sign_in
+     */
+    public Signin() {
+        initComponents();
+        Toolkit toolkit = getToolkit();
+        Dimension size = toolkit.getScreenSize();
+        setLocation(size.width/2-getWidth()/2, size.height/2-getHeight()/2);
+        
+        con = Db.mycon();
+    }
+
+    
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jPanel1 = new javax.swing.JPanel();
+        jusername = new javax.swing.JTextField();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jsign_in_ = new javax.swing.JButton();
+        jLabelsignup = new javax.swing.JLabel();
+        jpassword = new javax.swing.JPasswordField();
+        jLabel4 = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jPanel1.setBackground(new java.awt.Color(255, 51, 0));
+        jPanel1.setForeground(new java.awt.Color(255, 255, 255));
+        jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jusername.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
+        jPanel1.add(jusername, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 130, 245, 41));
+
+        jLabel1.setFont(new java.awt.Font("Segoe UI", 0, 28)); // NOI18N
+        jLabel1.setText("Username");
+        jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 130, 155, -1));
+
+        jLabel2.setFont(new java.awt.Font("Segoe UI", 0, 28)); // NOI18N
+        jLabel2.setText("Password");
+        jPanel1.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 200, 155, -1));
+
+        jLabel3.setFont(new java.awt.Font("Cambria", 1, 36)); // NOI18N
+        jLabel3.setText("Sign In");
+        jPanel1.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(170, 30, 120, 50));
+
+        jsign_in_.setBackground(new java.awt.Color(51, 51, 255));
+        jsign_in_.setFont(new java.awt.Font("Segoe UI", 0, 24)); // NOI18N
+        jsign_in_.setText("Sign In");
+        jsign_in_.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jsign_in_ActionPerformed(evt);
+            }
+        });
+        jPanel1.add(jsign_in_, new org.netbeans.lib.awtextra.AbsoluteConstraints(320, 270, 130, -1));
+
+        jLabelsignup.setBackground(new java.awt.Color(255, 255, 255));
+        jLabelsignup.setFont(new java.awt.Font("Segoe UI", 1, 22)); // NOI18N
+        jLabelsignup.setText("Create An Account ");
+        jLabelsignup.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        jLabelsignup.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jLabelsignupMouseClicked(evt);
+            }
+        });
+        jPanel1.add(jLabelsignup, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 320, 260, 50));
+
+        jpassword.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
+        jPanel1.add(jpassword, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 200, 245, 40));
+
+        jLabel4.setIcon(new javax.swing.ImageIcon("C:\\Users\\DELL\\Downloads\\pexels-pixabay-268415.jpg")); // NOI18N
+        jLabel4.setText("jLabel4");
+        jPanel1.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 480, 380));
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void jsign_in_ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jsign_in_ActionPerformed
+        // TODO add your handling code here:
+        String username = jusername.getText();
+        String password = jpassword.getText();
+        if(username.equals(""))
+        {
+            JOptionPane.showMessageDialog(null,"Add the Username");
+        }
+        else if(password.equals(""))
+        {
+            JOptionPane.showMessageDialog(null,"Add the Password");
+        }
+        else{  
+            
+            Encoder encoder = Base64.getEncoder();
+            String pw =  encoder.encodeToString(password.getBytes());
+            
+        try {
+        
+            String sql = " SELECT * FROM users WHERE username = ? AND password = ? ";
+            ps = con.prepareCall(sql);
+            ps.setString(1, username);
+            ps.setString(2, pw);
+            
+            rs = ps.executeQuery();
+   
+           
+            if (rs.next()){
+                JOptionPane.showMessageDialog(rootPane, "You're signin....");
+            }else    
+            {
+                JOptionPane.showMessageDialog(rootPane, "Your signin failed");
+            }
+        } catch (Exception e) {
+        }       
+    }//GEN-LAST:event_jsign_in_ActionPerformed
+    }
+    private void jLabelsignupMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLabelsignupMouseClicked
+        // signing form show:
+
+        Signup su = new Signup();
+        su.setVisible(true);
+        su.pack();
+        su.setLocationRelativeTo(null);
+        this.setVisible(false);
+    }//GEN-LAST:event_jLabelsignupMouseClicked
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(Signin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(Signin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(Signin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Signin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Signin().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabelsignup;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPasswordField jpassword;
+    private javax.swing.JButton jsign_in_;
+    private javax.swing.JTextField jusername;
+    // End of variables declaration//GEN-END:variables
+}
